abb93257cbe1b71e3461fc2c21719a2dda1432dd
Bhupesh Prajapati <sciencebhupesh1997@gmail.com>
2020-04-24 15:06:09 +0530

    Initializing Expense Tracker with Pycharm virtual environment- new project

---
+++ Main.py
@@ -0,0 +1,4 @@
+########################################################################################################
+#python project : Expense Tracker to track my expences and save my data in cloud or database or a file
+# Author: Bhupesh Prajapati
+########################################################################################################
